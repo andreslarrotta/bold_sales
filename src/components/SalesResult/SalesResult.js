823d48396cb2b98173eea32e5772a66db38255b3
Andres Felipe Larrotta Pino <andres.larrota@isobar.com>
2023-01-26 12:11:17 -0500

    feat(src): tabla dinamica terminada con vistas de hoy, semana y mes de septiembre

--- src/components/SalesResult/SalesResult.js
+++ src/components/SalesResult/SalesResult.js
@@ -1,14 +1,37 @@
 import React from 'react'
 
 import './SalesResult.css'
-import paymentIcon from '../../assets/mastercard_icon.png'
+import paymentIconMaster from '../../assets/mastercard_icon.png'
+import paymentIconVisa from '../../assets/visa_icon.png'
+import { useSales } from '../../context/sales-context'
 
 export const SalesResult = () => {
+    const { dataFilter, view, week } = useSales()
+
+    console.log('data para la table', dataFilter)
+    const handlePrice = (amount) => {
+        const formatter = new Intl.NumberFormat('es-CO', {
+            style: 'currency',
+            currency: 'COP',
+        });
+        return formatter.format(amount)
+    }
 
     return (
         <div className='sales_result'>
             <div className='sales_result--title'>
-                Tus ventas de septiembre
+                Tus ventas de
+                {
+                    view === 'Septiembre' && ' septiembre'
+                }
+
+                {
+                    view === 'Week' && ` la semana ${week}`
+                }
+
+                {
+                    view === 'Today' && ' hoy'
+                }
             </div>
             <div className='sales_result--table'>
                 <table cellSpacing="0" cellPadding="0">
@@ -20,92 +43,38 @@ export const SalesResult = () => {
                             <td>ID transacción Bold</td>
                             <td>Monto</td>
                         </tr>
-                        <tr className='sales_result--table-content'>
-                            <td className='sales_result--table-content_transaccion'>
-                                <span className="material-symbols-outlined">
-                                    phone_iphone
-                                </span> Cobro exitoso
-                            </td>
-                            <td className='sales_result--table-content_date'>
-                                04/06/2020 - 17:14:24
-                            </td>
-                            <td className='sales_result--table-content_payment'>
-                                <img src={paymentIcon} alt='' />
-                                *** *** *** 7711
-                            </td>
-                            <td className='sales_result--table-content_id'>
-                                GZEN23784UBV2
-                            </td>
-                            <td className='sales_result--table-content_amount'>
-                                <span className='sales_result--table-content_amount--total'>$25.000</span>
-                                <span className='sales_result--table-content_amount--text'>Deducción Bold</span>
-                                <span className='sales_result--table-content_amount--discount'>-$1.5000</span>
-                            </td>
-                        </tr>
-                        <tr className='sales_result--table-content'>
-                            <td className='sales_result--table-content_transaccion'>
-                                <span className="material-symbols-outlined">
-                                    phone_iphone
-                                </span> Cobro no realizado
-                            </td>
-                            <td className='sales_result--table-content_date'>
-                                04/06/2020 - 17:14:24
-                            </td>
-                            <td className='sales_result--table-content_payment'>
-                                <img src={paymentIcon} alt='' />
-                                *** *** *** 7711
-                            </td>
-                            <td className='sales_result--table-content_id'>
-                                GZEN23784UBV2
-                            </td>
-                            <td className='sales_result--table-content_amount'>
-                                <span className='sales_result--table-content_amount--total'>$15.000</span>
-                            </td>
-                        </tr>
-                        <tr className='sales_result--table-content'>
-                            <td className='sales_result--table-content_transaccion'>
-                                <span className="material-symbols-outlined">
-                                    attach_file
-                                </span> Cobro exitoso
-                            </td>
-                            <td className='sales_result--table-content_date'>
-                                04/06/2020 - 17:14:24
-                            </td>
-                            <td className='sales_result--table-content_payment'>
-                                <img src={paymentIcon} alt='' />
-                                *** *** *** 7711
-                            </td>
-                            <td className='sales_result--table-content_id'>
-                                GZEN23784UBV2
-                            </td>
-                            <td className='sales_result--table-content_amount'>
-                                <span className='sales_result--table-content_amount--total'>$25.000</span>
-                                <span className='sales_result--table-content_amount--text'>Deducción Bold</span>
-                                <span className='sales_result--table-content_amount--discount'>-$1.5000</span>
-                            </td>
-                        </tr>
-                        <tr className='sales_result--table-content'>
-                            <td className='sales_result--table-content_transaccion'>
-                                <span className="material-symbols-outlined">
-                                    phone_iphone
-                                </span> Cobro exitoso
-                            </td>
-                            <td className='sales_result--table-content_date'>
-                                04/06/2020 - 17:14:24
-                            </td>
-                            <td className='sales_result--table-content_payment'>
-                                <img src={paymentIcon} alt='' />
-                                *** *** *** 7711
-                            </td>
-                            <td className='sales_result--table-content_id'>
-                                GZEN23784UBV2
-                            </td>
-                            <td className='sales_result--table-content_amount'>
-                                <span className='sales_result--table-content_amount--total'>$25.000</span>
-                                <span className='sales_result--table-content_amount--text'>Deducción Bold</span>
-                                <span className='sales_result--table-content_amount--discount'>-$1.5000</span>
-                            </td>
-                        </tr>
+                        {
+                            dataFilter.map((sale, index) => {
+                                return <tr className='sales_result--table-content' key={`${sale.id}-${index}`}>
+                                    <td className='sales_result--table-content_transaccion'>
+                                        <span className="material-symbols-outlined">
+                                            {sale.icon}
+                                        </span> {sale.transaction}
+                                    </td>
+                                    <td className='sales_result--table-content_date'>
+                                        {sale.date}
+                                    </td>
+                                    <td className='sales_result--table-content_payment'>
+                                        <img src={sale.paymentMethod.franchise === 'MasterCard' ? paymentIconMaster : sale.paymentMethod.franchise === 'Visa' ? paymentIconVisa : paymentIconMaster} alt='' />
+                                        {sale.paymentMethod.number}
+                                    </td>
+                                    <td className='sales_result--table-content_id'>
+                                        {sale.id}
+                                    </td>
+                                    <td className='sales_result--table-content_amount'>
+                                        {
+                                            sale.amount.total && <span className='sales_result--table-content_amount--total'>{handlePrice(sale.amount.total)}</span>
+                                        }
+                                        {
+                                            sale.amount.title && <span className='sales_result--table-content_amount--text'>{sale.amount.title}</span>
+                                        }
+                                        {
+                                            sale.amount.discounts !== 0 && <span className='sales_result--table-content_amount--discount'>-{handlePrice(sale.amount.discounts)}</span>
+                                        }
+                                    </td>
+                                </tr>
+                            })
+                        }
                     </tbody>
                 </table>
             </div>
